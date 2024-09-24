dd75b1dccc40062f864d3d3531f6264420b8e6d2
Ravindu Deshan <Droidcasts@protonmail.com>
2024-09-24 20:41:23 +0530

    Added a function to download the required toolchain

--- build_kernel_GKI.sh
+++ build_kernel_GKI.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+RDIR="$(pwd)"
+
+# 0. Downloading toolchain
+if [ ! -d "${RDIR}/kernel_platform/prebuilts" ]; then
+    echo -e "[+] Downloading Toolchain...\n"
+    curl -LO --progress-bar https://github.com/ravindu644/a05s_stock/releases/download/toolchain/toolchain.zip
+    curl -LO --progress-bar https://github.com/ravindu644/a05s_stock/releases/download/toolchain/toolchain.z01
+    zip -s- toolchain.zip -O combined.zip && unzip combined.zip && rm combined.zip
+    tar -xvf toolchain.tar.gz
+    mv prebuilts "${RDIR}/kernel_platform" && chmod +x -R "${RDIR}/kernel_platform/prebuilts"
+    rm toolchain*
+else
+    echo -e "[+] Toolchain already installed...\n"    
+fi
 
 #1. target config
 BUILD_TARGET=$1
