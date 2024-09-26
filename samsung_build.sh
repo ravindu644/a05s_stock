e0787dfac3f718b9031a3215d7fafef7f3928946
Ravindu Deshan <Droidcasts@protonmail.com>
2024-09-26 06:59:34 +0000

    build script: install requirements at the start

--- samsung_build.sh
+++ samsung_build.sh
@@ -2,12 +2,12 @@
 
 # Check if the script is running with sudo
 if [ "$EUID" -ne 0 ]; then
-    echo "[+] Script is not running with sudo, trying to acquire sudo permissions..."
+    echo -e  "[+] Script is not running with sudo, trying to acquire sudo permissions...\n"
     exec sudo "$0" "$@"
 fi
 
 # Create a log file and print start time
-echo "Build started at: $(date)" | tee logs.txt
+echo -e "Build started at: $(date)\n" | tee logs.txt
 
 # OEM Variables
 export ANDROID_BUILD_TOP="$(pwd)"
@@ -54,6 +54,15 @@ if [ ! -d "${ANDROID_BUILD_TOP}/kernel_platform/prebuilts" ]; then
     sudo apt install rsync > /dev/null 2>&1
 else
     echo -e "[+] Toolchain already installed...\n"  
+fi
+
+# requirements
+if [ ! -f .requirements ]; then
+    echo -e "[+] Installing Requirements...\n"
+    sudo apt update -y ; sudo apt install default-jdk git-core gnupg flex bison gperf build-essential zip curl libc6-dev libncurses5-dev x11proto-core-dev libx11-dev libreadline6-dev libgl1-mesa-glx libgl1-mesa-dev python3 make sudo gcc g++ bc grep tofrodos python3-markdown libxml2-utils xsltproc zlib1g-dev libncurses5 python-is-python3 libc6-dev libtinfo5 ncurses-dev make python2 cpio kmod openssl libelf-dev dwarves libssl-dev libelf-dev -y
+    echo 1 > .requirements
+else
+    echo -e "[+] Requirements already installed...\n"      
 fi
 
 #main execution
